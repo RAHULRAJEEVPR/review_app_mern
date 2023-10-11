0e3002eaf6ec2bc2d0d3fe274eea57f004a8e10c
Rahul P R <rahulrajeevpr@gmail.com>
2023-10-11 21:40:16 +0530

    cleard some test case in express validator

--- backEnd/middlewares/validator.js
+++ backEnd/middlewares/validator.js
@@ -4,19 +4,20 @@ const mongoSanitize = require("express-mongo-sanitize");
 const reviewValidation = [
   body("usage").isString().notEmpty(),
   body("goals").isArray().notEmpty(),
-  body("rating").isNumeric().notEmpty(),
+  body("rating").isNumeric().notEmpty(), 
   body("suggestion").optional().isString(),
   body("birthday").isISO8601().toDate(),
 ];
 
 const validate = (req, res, next) => {
+  
   mongoSanitize.sanitize(req.body);
 
   const errors = validationResult(req);
   if (!errors.isEmpty()) {
     return res.status(400).json({ errors: errors.array() });
+    
   }
-  console.log(errors);
   next();
 };
 
